8fa5f595e5244fbb17701a750247e0d88d3b7c5f
temple69 <utemple169@gmail.com>
2022-11-15 18:02:41 +0100

    pages that renders the homepage in the home route

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Discover from '../../Components/Discover/Discover'
+import Icons from '../../Components/Icons/Icons'
+import Inspiration from '../../Components/Inspiration/Inspiration'
+import Landing from '../../Components/Landing/Landing'
+
+
+ const Home = () => {
+  return (
+    <main>
+        <Landing/>
+        <Icons/>
+        <Inspiration/>
+        <Discover/>
+    </main>
+  )
+}
+export default Home
