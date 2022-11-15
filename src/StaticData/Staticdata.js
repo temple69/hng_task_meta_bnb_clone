4822af45be12ae0252f3fcbd0e2382de4b459003
temple69 <utemple169@gmail.com>
2022-11-15 18:05:39 +0100

    folder which contains static data and image url in json format

---
+++ src/StaticData/Staticdata.js
@@ -0,0 +1,131 @@
+import img1 from '../assets/inspiration/img_1.png'
+import img2 from '../assets/inspiration/img_2.png'
+import img3 from '../assets/inspiration/img_3.png'
+import img4 from '../assets/inspiration/img_4.png'
+import img5 from '../assets/inspiration/img_5.png'
+import img6 from '../assets/inspiration/img_6.png'
+import img7 from '../assets/inspiration/img_7.png'
+import img8 from '../assets/inspiration/img_8.png'
+import img9 from '../assets/inspiration/img_9.png'
+import img10 from '../assets/inspiration/img_10.png'
+import img11 from '../assets/inspiration/img_11.png'
+import img12 from '../assets/inspiration/img_12.png'
+import img13 from '../assets/inspiration/img_13.png'
+import img14 from '../assets/inspiration/img_14.png'
+import img15 from '../assets/inspiration/img_15.png'
+import img16 from '../assets/inspiration/img_16.png'
+export const inspirationData = [{
+        id: "1",
+        imgurl: img1,
+    },
+    {
+        id: "2",
+        imgurl: img2,
+
+    },
+    {
+        id: "3",
+        imgurl: img3
+
+    },
+    {
+        id: "4",
+        imgurl: img4
+
+    },
+    {
+        id: "5",
+        imgurl: img5
+
+    },
+    {
+        id: "6",
+        imgurl: img6
+
+    },
+    {
+        id: "7",
+        imgurl: img7
+
+    },
+    {
+        id: "8",
+        imgurl: img8
+
+    },
+
+]
+export const PlacesData = [{
+        id: "9",
+        imgurl: img5
+    },
+    {
+        id: "10",
+        imgurl: img6
+    },
+    {
+        id: "11",
+        imgurl: img7
+    },
+    {
+        id: "12",
+        imgurl: img8
+    },
+    {
+        id: "13",
+        imgurl: img1
+    },
+    {
+        id: "14",
+        imgurl: img2
+    },
+    {
+        id: "15",
+        imgurl: img3
+    },
+    {
+
+        id: "16",
+        imgurl: img4
+    },
+
+    {
+        id: "17",
+        imgurl: img9
+
+    },
+    {
+        id: "18",
+        imgurl: img10
+
+    }, {
+        id: "19",
+        imgurl: img11
+
+    },
+    {
+        id: "20",
+        imgurl: img12
+
+    },
+    {
+        id: "21",
+        imgurl: img13
+
+    },
+    {
+        id: "22",
+        imgurl: img14,
+
+    },
+    {
+        id: "23",
+        imgurl: img15,
+
+    },
+    {
+        id: "24",
+        imgurl: img16,
+
+    }
+]
