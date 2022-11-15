0e653719244a0a20adcb73caa349731c33bb2fa3
temple69 <utemple169@gmail.com>
2022-11-15 18:04:41 +0100

    page that renders the places pages in the place-to-stay route

---
+++ src/Pages/Places/NftPlace.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import Places from '../../Components/Places/Places'
+
+
+
+const NftPlace = () => {
+  return (
+    <main>
+        <Navbar/>
+        <Places/>
+    </main>
+  )
+}
+
+export default NftPlace
